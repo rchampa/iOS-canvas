d6649a2340e24eed1b92bb5b2101aeca7afc3490
rchampa <ricardo.champa@gmail.com>
2018-04-10 16:30:30 +0200

    primer commit

--- Firma/ViewController.swift
+++ Firma/ViewController.swift
@@ -9,17 +9,119 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var canvas: UIImageView!
+    
+    //stores the last drawn point on the canvas. This is used when a continuous brush stroke is being drawn on the canvas.
+    var lastPoint = CGPoint.zero
+    
+    //the current RGB values of the selected color.
+    var red: CGFloat = 0.0
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+    
+    //the brush stroke width and opacity
+    var brushWidth: CGFloat = 10.0
+    
+    //the brush opacity
+    var opacity: CGFloat = 1.0
+    
+    //identifies if the brush stroke is continuous
+    var swiped = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setBlackColor()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func setBlackColor() {
+        red = 0
+        green = 0
+        blue = 0
     }
-
-
+    //UIViewController is a subclass of UIResponder
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = false
+        if let touch = touches.first {
+            lastPoint = touch.location(in: self.view)
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if !swiped {
+            // draw a single point
+            drawLineFrom(fromPoint: lastPoint, toPoint: lastPoint)
+        }
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?){
+        // 6
+        swiped = true
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: view)
+            drawLineFrom(fromPoint: lastPoint, toPoint: currentPoint)
+            
+            // 7
+            lastPoint = currentPoint
+        }
+    }
+    
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+        
+        // 1
+        UIGraphicsBeginImageContext(view.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        canvas.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+        
+        // 2
+        let fPoint = CGPoint(x:fromPoint.x, y:fromPoint.y)
+        let tPoint = CGPoint(x:toPoint.x, y:toPoint.y)
+        context?.move(to: fPoint)
+        context?.addLine(to: tPoint)
+        
+        // 3
+        context?.setLineCap(CGLineCap.round)
+        context?.setLineWidth(brushWidth)
+        context?.setStrokeColor(red: red, green: green, blue: blue, alpha: 1.0)
+        context?.setBlendMode(CGBlendMode.normal)
+        
+        // 4
+        context?.strokePath()
+        
+        // 5
+        canvas.image = UIGraphicsGetImageFromCurrentImageContext()
+        canvas.alpha = opacity
+        UIGraphicsEndImageContext()
+        
+    }
 }
+private extension ViewController{
+    @IBAction func exportSign(_ sender: Any) {
+        
+        UIGraphicsBeginImageContext(canvas.bounds.size)
+        canvas.image?.draw(in: CGRect(x: 0, y: 0,width: canvas.frame.size.width, height: canvas.frame.size.height))
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        
+//        let activity = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+//        optionMenu.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+//        present(activity, animated: true, completion: nil)
+        
+        let objectsToShare: [AnyObject] = [image!]
+        let activityViewController = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        activityViewController.excludedActivityTypes = [ UIActivityType.airDrop]
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func clearCanvas(_ sender: Any) {
+        canvas.image = nil
+    }
+    
+}
+
 
