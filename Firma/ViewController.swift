4fc739a45f3110fb480abcf9de3bec257070f6d8
rchampa <ricardo.champa@gmail.com>
2018-04-10 10:46:33 +0200

    Initial Commit

---
+++ Firma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firma
+//
+//  Created by Ricardo Champa on 10/04/2018.
+//  Copyright © 2018 Ricardo Champa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
